c1f7bef09468f8392e902a8b66172d22c153978f
Fathan Margono <fmgonoo@gmail.com>
2021-03-20 12:26:05 +0700

    Update:SwAlert - change style border to be shape and add type 'outlined'

--- src/components/atoms/SwAlert.svelte
+++ src/components/atoms/SwAlert.svelte
@@ -1,33 +1,46 @@
 <script lang="ts">
-  export let type: string  = ''
+  type FilledType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info'
+  type OutlinedType = 'primary-outlined' | 'secondary-outlined' | 'success-outlined' | 'danger-outlined' | 'warning-outlined' | 'info-outlined'
+  export let type: (FilledType | OutlinedType) = 'primary'
   export let title: string  = ''
   export let description: string  = ''
 </script>
 
 <div 
-  class="w-full p-3 m-1 rounded-l-md rounded-r-lg shadow-lg"
+  class="w-full p-3 m-1 rounded-l-md rounded-r-lg shadow-lg flex"
   class:primary="{type === 'primary'}"
+  class:primary-outlined="{type === 'primary-outlined'}"
   class:secondary="{type === 'secondary'}"
+  class:secondary-outlined="{type === 'secondary-outlined'}"
   class:success="{type === 'success'}"
+  class:success-outlined="{type === 'success-outlined'}"
   class:danger="{type === 'danger'}"
+  class:danger-outlined="{type === 'danger-outlined'}"
   class:warning="{type === 'warning'}"
+  class:warning-outlined="{type === 'warning-outlined'}"
   class:info="{type === 'info'}"
+  class:info-outlined="{type === 'info-outlined'}"
   
   >
+  <!-- Shape -->
   <div 
-    class="border-l-2 pl-3"
-    class:text-primary-600="{type === 'primary'}"
-    class:text-secondary-600="{type === 'secondary'}"
-    class:text-success-600="{type === 'success'}"
-    class:text-danger-600="{type === 'danger'}"
-    class:text-warning-600="{type === 'warning'}"
-    class:text-info-600="{type === 'info'}"
-    class:border-primary-600="{type === 'primary'}"
-    class:border-secondary-600="{type === 'secondary'}"
-    class:border-success-600="{type === 'success'}"
-    class:border-danger-600="{type === 'danger'}"
-    class:border-warning-600="{type === 'warning'}"
-    class:border-info-600="{type === 'info'}">
+    class="h-auto w-1 rounded-lg"
+    class:bg-primary-600="{type === 'primary' || type === 'primary-outlined' }"
+    class:bg-secondary-600="{type === 'secondary' || type === 'secondary-outlined' }"
+    class:bg-success-600="{type === 'success' || type === 'success-outlined' }"
+    class:bg-danger-600="{type === 'danger' || type === 'danger-outlined' }"
+    class:bg-warning-600="{type === 'warning' || type === 'warning-outlined' }"
+    class:bg-info-600="{type === 'info' || type === 'info-outlined' }">
+  </div>
+  <div 
+    class="pl-3"
+    class:text-primary-600="{type === 'primary' || type === 'primary-outlined' }"
+    class:text-secondary-600="{type === 'secondary' || type === 'secondary-outlined' }"
+    class:text-success-600="{type === 'success' || type === 'success-outlined' }"
+    class:text-danger-600="{type === 'danger' || type === 'danger-outlined' }"
+    class:text-warning-600="{type === 'warning' || type === 'warning-outlined' }"
+    class:text-info-600="{type === 'info' || type === 'info-outlined' }"
+    >
     <div class="font-semibold">
       { title }
     </div>
@@ -41,19 +54,37 @@
   .primary {
     @apply bg-primary-200 text-primary-600;
   }
+  .primary-outlined {
+    @apply border-2 border-primary-200 text-primary-600;
+  }
   .secondary {
     @apply bg-secondary-200 text-secondary-600;
+  }
+  .secondary-outlined {
+    @apply border-2 border-secondary-200 text-secondary-600;
   }
   .success {
     @apply bg-success-200 text-success-600;
   }
+  .success-outlined {
+    @apply border-2 border-success-200 text-success-600;
+  }
   .danger {
     @apply bg-danger-200 text-danger-600;
+  }
+  .danger-outlined {
+    @apply border-2 border-danger-200 text-danger-600;
   }
   .info {
     @apply bg-info-200 text-info-600;
   }
+  .info-outlined {
+    @apply border-2 border-info-200 text-info-600;
+  }
   .warning {
     @apply bg-warning-200 text-warning-600;
+  }
+  .warning-outlined {
+    @apply border-2 border-warning-200 text-warning-600;
   }
 </style>
