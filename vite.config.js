a36b63b34e707ad0217d42a3bb5476f06204ae2b
Fathan Margono <fmgonoo@gmail.com>
2021-03-21 16:17:31 +0700

    Add:icon - Add svelte-hero-icons package

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import svelte from '@svitejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  optimizeDeps: {
+    include: ['svelte-hero-icons'],
+  }
 })
