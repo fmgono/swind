391afe48c86869213a72acf0aec7e1e5579b0991
Fathan Margono <fmgonoo@gmail.com>
2021-03-21 18:23:22 +0700

    Update:SwAlert - Add props icon, default icon, showIcon state

--- src/components/atoms/SwAlert.svelte
+++ src/components/atoms/SwAlert.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
+  import type { SvelteComponentTyped } from "svelte"
+  import Icon, { InformationCircle, ExclamationCircle, CheckCircle, XCircle } from 'svelte-hero-icons'
+
   type FilledType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info'
   type OutlinedType = 'primary-outlined' | 'secondary-outlined' | 'success-outlined' | 'danger-outlined' | 'warning-outlined' | 'info-outlined'
+  /**
+   * Type of Alert style
+   */
   export let type: (FilledType | OutlinedType) = 'primary'
+  /**
+   * Title for alert
+   */
   export let title: string  = ''
+  /**
+   * Additional text
+   */
   export let description: string  = ''
+  /**
+   * Showing icon or not
+   */
+  export let showIcon: boolean  = false
+  /**
+   * Showing icon or not
+   */
+  export let icon: SvelteComponentTyped = undefined
+
+  let defaultIcon: any
+
+  $: if (type.includes('success')) {
+    defaultIcon = CheckCircle
+  } else if (type.includes('danger')) {
+    defaultIcon = XCircle
+  } else if (type.includes('warning')) {
+    defaultIcon = ExclamationCircle
+  } else if (type.includes('info')) {
+    defaultIcon = InformationCircle
+  } else {
+    defaultIcon = InformationCircle
+  }
 
   $: primary = type === 'primary'
   $: primaryOutlined = type === 'primary-outlined'
@@ -23,7 +57,7 @@
   class="w-full p-3 m-1 rounded-md shadow-lg flex {type}">
   <!-- Shape -->
   <div 
-    class="h-auto w-1 rounded-lg"
+    class="min-h-full w-1 rounded-lg mr-2"
     class:bg-primary-600="{primary || primaryOutlined}"
     class:bg-secondary-600="{secondary || secondaryOutlined}"
     class:bg-success-600="{success || successOutlined}"
@@ -31,21 +65,27 @@
     class:bg-warning-600="{warning || warningOutlined}"
     class:bg-info-600="{info || infoOutlined}">
   </div>
-  <div 
-    class="pl-3"
-    class:text-primary-600="{primary || primaryOutlined}"
-    class:text-secondary-600="{secondary || secondaryOutlined}"
-    class:text-success-600="{success || successOutlined}"
-    class:text-danger-600="{danger || dangerOutlined}"
-    class:text-warning-600="{warning || warningOutlined}"
-    class:text-info-600="{info || infoOutlined }"
-    >
-    <div class="font-semibold">
-      { title }
+  <div class="flex items-center">
+    {#if (showIcon)}
+    <Icon src="{icon ? icon : defaultIcon}" solid size="24" />
+    {/if}
+    <div
+      class="ml-2"
+      class:text-primary-600="{primary || primaryOutlined}"
+      class:text-secondary-600="{secondary || secondaryOutlined}"
+      class:text-success-600="{success || successOutlined}"
+      class:text-danger-600="{danger || dangerOutlined}"
+      class:text-warning-600="{warning || warningOutlined}"
+      class:text-info-600="{info || infoOutlined }"
+      >
+      <div class="font-semibold">
+        { title }
+      </div>
+      <span class="text-sm">
+        { description }
+      </span>
     </div>
-    <span class="text-sm">
-      { description }
-    </span>
+
   </div>
 </div>
 
@@ -54,36 +94,36 @@
     @apply bg-primary-200 text-primary-600;
   }
   .primary-outlined {
-    @apply ring-2 ring-primary-200 text-primary-600;
+    @apply ring-inset ring-2 ring-primary-200 text-primary-600;
   }
   .secondary {
     @apply bg-secondary-200 text-secondary-600;
   }
   .secondary-outlined {
-    @apply ring-2 ring-secondary-200 text-secondary-600;
+    @apply ring-inset ring-2 ring-secondary-200 text-secondary-600;
   }
   .success {
     @apply bg-success-200 text-success-600;
   }
   .success-outlined {
-    @apply ring-2 ring-success-200 text-success-600;
+    @apply ring-inset ring-2 ring-success-200 text-success-600;
   }
   .danger {
     @apply bg-danger-200 text-danger-600;
   }
   .danger-outlined {
-    @apply ring-2 ring-danger-200 text-danger-600;
+    @apply ring-inset ring-2 ring-danger-200 text-danger-600;
   }
   .info {
     @apply bg-info-200 text-info-600;
   }
   .info-outlined {
-    @apply ring-2 ring-info-200 text-info-600;
+    @apply ring-inset ring-2 ring-info-200 text-info-600;
   }
   .warning {
     @apply bg-warning-200 text-warning-600;
   }
   .warning-outlined {
-    @apply ring-2 ring-warning-200 text-warning-600;
+    @apply ring-inset ring-2 ring-warning-200 text-warning-600;
   }
 </style>
