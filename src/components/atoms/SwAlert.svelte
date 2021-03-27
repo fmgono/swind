a0ad2b147e8b9999aab68495a7ff8f8bae8b06b0
Fathan Margono <fmgonoo@gmail.com>
2021-03-27 11:56:29 +0700

    Rewrite:SwAlert - Rewrite SwAlert component with SwBox and SwText

--- src/components/atoms/SwAlert.svelte
+++ src/components/atoms/SwAlert.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-  import type { SvelteComponentTyped } from "svelte"
   import Icon, { InformationCircle, ExclamationCircle, CheckCircle, XCircle } from 'svelte-hero-icons'
   import SwBox from './SwBox.svelte'
+  import SwText from './SwText.svelte'
 
-  type FilledType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info'
-  type OutlinedType = 'primary-outlined' | 'secondary-outlined' | 'success-outlined' | 'danger-outlined' | 'warning-outlined' | 'info-outlined'
+  type Type = 'success' | 'danger' | 'warning' | 'info'
+  type VariantType = 'vertical' | 'horizontal'
+  
   /**
    * Type of Alert style
    */
-  export let type: (FilledType | OutlinedType) = 'primary'
+  export let type: Type = 'info'
   /**
-   * Title for alert
+   * Variant of Alert style
    */
-  export let title: string  = ''
+  export let variant: VariantType = 'horizontal'
+  /**
+   * Message for alert
+   */
+  export let message: string  = ''
   /**
    * Additional text
    */
@@ -21,118 +26,40 @@
    * Showing icon or not
    */
   export let showIcon: boolean  = false
-  /**
-   * Showing icon or not
-   */
-  export let icon: SvelteComponentTyped = undefined
 
-  let defaultIcon: any
+  let icon: any
 
   $: if (type.includes('success')) {
-    defaultIcon = CheckCircle
+    icon = CheckCircle
   } else if (type.includes('danger')) {
-    defaultIcon = XCircle
+    icon = XCircle
   } else if (type.includes('warning')) {
-    defaultIcon = ExclamationCircle
-  } else if (type.includes('info')) {
-    defaultIcon = InformationCircle
+    icon = ExclamationCircle
   } else {
-    defaultIcon = InformationCircle
+    icon = InformationCircle
   }
 
-  $: primary = type === 'primary'
-  $: primaryOutlined = type === 'primary-outlined'
-  $: secondary = type === 'secondary'
-  $: secondaryOutlined = type === 'secondary-outlined'
-  $: success = type === 'success'
-  $: successOutlined = type === 'success-outlined'
-  $: danger = type === 'danger'
-  $: dangerOutlined = type === 'danger-outlined'
-  $: warning = type === 'warning'
-  $: warningOutlined = type === 'warning-outlined'
-  $: info = type === 'info'
-  $: infoOutlined = type === 'info-outlined'
+  $: direction = variant === 'horizontal' ? 'row' : 'col'
+  $: align = variant === 'horizontal' ? 'center' : 'start'
+  $: console.log(direction, align)
+
 </script>
 
-<SwBox role="alert" w="full" p="3" m="1" borderRadius="md" shadow="lg" display="flex">
-  <SwBox minH="screen" w="1" borderRadius="lg" mr="2" bgColor="blue" />
-  <SwBox display="flex" align="center">
-
+<SwBox role="alert" w="full" p="3" m="1" rounded="md" border="2" borderColor="{`${type}-light`}" bgColor="{`${type}-50`}" shadow="lg">
+  {#if variant === 'vertical'}
+  <SwBox w="1" rounded="lg" mr="3" bgColor="{`${type}-light`}" />
+  {/if}
+  
+  <SwBox display="flex" align="center" color="{`${type}-light`}">
+    {#if (showIcon)}
+    <Icon src="{icon}" solid size="24" />
+    {/if}
+    <SwBox ml="3" color="current" display="flex" flexDir={direction} align={align}>
+      <!-- <div class="font-semibold">
+        { message }
+      </div> -->
+      <SwText weight="semibold" > { message} </SwText>
+      <SwText size="sm" class="{variant === 'horizontal' && 'ml-3'}"> { description} </SwText>
+    </SwBox>
   </SwBox>
 </SwBox>
-
-<div
-  role="alert"
-  class="w-full p-3 m-1 rounded-md shadow-lg flex {type}">
-  <!-- Shape -->
-  <div 
-    class="min-h-full w-1 rounded-lg mr-2"
-    class:bg-primary-600="{primary || primaryOutlined}"
-    class:bg-secondary-600="{secondary || secondaryOutlined}"
-    class:bg-success-600="{success || successOutlined}"
-    class:bg-danger-600="{danger || dangerOutlined}"
-    class:bg-warning-600="{warning || warningOutlined}"
-    class:bg-info-600="{info || infoOutlined}">
-  </div>
-  <div class="flex items-center">
-    {#if (showIcon)}
-    <Icon src="{icon ? icon : defaultIcon}" size="24" />
-    {/if}
-    <div
-      class="ml-2"
-      class:text-primary-600="{primary || primaryOutlined}"
-      class:text-secondary-600="{secondary || secondaryOutlined}"
-      class:text-success-600="{success || successOutlined}"
-      class:text-danger-600="{danger || dangerOutlined}"
-      class:text-warning-600="{warning || warningOutlined}"
-      class:text-info-600="{info || infoOutlined }"
-      >
-      <div class="font-semibold">
-        { title }
-      </div>
-      <span class="text-sm">
-        { description }
-      </span>
-    </div>
-
-  </div>
-</div>
-
-<!-- <style>
-  .primary {
-    @apply bg-primary-200 text-primary-600;
-  }
-  .primary-outlined {
-    @apply ring-inset ring-2 ring-primary-200 text-primary-600;
-  }
-  .secondary {
-    @apply bg-secondary-200 text-secondary-600;
-  }
-  .secondary-outlined {
-    @apply ring-inset ring-2 ring-secondary-200 text-secondary-600;
-  }
-  .success {
-    @apply bg-success-200 text-success-600;
-  }
-  .success-outlined {
-    @apply ring-inset ring-2 ring-success-200 text-success-600;
-  }
-  .danger {
-    @apply bg-danger-200 text-danger-600;
-  }
-  .danger-outlined {
-    @apply ring-inset ring-2 ring-danger-200 text-danger-600;
-  }
-  .info {
-    @apply bg-info-200 text-info-600;
-  }
-  .info-outlined {
-    @apply ring-inset ring-2 ring-info-200 text-info-600;
-  }
-  .warning {
-    @apply bg-warning-200 text-warning-600;
-  }
-  .warning-outlined {
-    @apply ring-inset ring-2 ring-warning-200 text-warning-600;
-  }
-</style> -->
